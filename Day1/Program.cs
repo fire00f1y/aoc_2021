154365ebc27b5d23dce988dff296088de469e192
Bryan Davis <fire00f1y@gmail.com>
2021-12-01 13:09:07 -0500

    finished day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,55 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.IO;
 
-namespace Calculator
+namespace Day1
 {
     class Program
     {
+        private static List<int> sums = new List<int>();
+
         static void Main(string[] args)
         {
-            var num1 = 0;
-            var num2 = 0;
+            var reader = new StreamReader(File.OpenRead("input.txt"));
+            var increased = 0;
 
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("-----------------------\n");
-
-            Console.WriteLine("Type a number, and then press Enter");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Type another number, and then press Enter");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Choose an option from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            Loop:
-            switch (Console.ReadLine())
+            var window = new SlidingWindow(3);
+            while (!reader.EndOfStream)
             {
-                case "a":
-                    Console.WriteLine($"Your result: {num1} + {num2} = {num1 + num2}");
-                    break;
-                case "s":
-                    Console.WriteLine($"Your result: {num1} - {num2} = {num1 - num2}");
-                    break;
-                case "m":
-                    Console.WriteLine($"Your result: {num1} * {num2} = {num1 * num2}");
-                    break;
-                case "d":
-                    Console.WriteLine($"Your result: {num1} / {num2} = {num1 / num2}");
-                    break;
-                default:
-                    Console.WriteLine("Invalid option. Please try again.");
-                    goto Loop;
+                // Assuming no input is malformed
+                var line = reader.ReadLine();
+                var num = Convert.ToInt32(line);
+                window.Add(num);
+
+                if (!window.Summable())
+                {
+                    continue;
+                }
+
+                var sum = window.Sum();
+                if (sums.Count > 0)
+                {
+                    increased += sums[^1] < sum ? 1 : 0;
+                }
+                sums.Add(sum);
             }
-            
-            Console.Write("Press any key to close the Calculator console app...");
-            Console.ReadKey();
+
+            Console.WriteLine($"{increased} number of depth increases");
         }
     }
 }
---
+++ Day1/SlidingWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1
+{
+    public class SlidingWindow
+    {
+        private readonly List<int> _window;
+        private int _cursor = 0;
+        private readonly int _max;
+        private bool _full = false;
+
+        public SlidingWindow(int size)
+        {
+            _window = new List<int>(size);
+            for (int i = 0; i < size; i++)
+            {
+                _window.Add(0);
+            }
+            _max = size;
+        }
+
+        public void Add(int num)
+        {
+            _window[_cursor] = num;
+            _cursor++;
+            if (_cursor < _max) return;
+            _full = true;
+            _cursor = 0;
+        }
+
+        public bool Summable()
+        {
+            return _full;
+        }
+
+        public int Sum()
+        {
+            return _window.AsQueryable().Sum();
+        }
+    }
+}
