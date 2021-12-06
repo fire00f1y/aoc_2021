5728e29e2f0d0c4a3baa1ec9e7f1738fb1b52e1c
Bryan Davis <fire00f1y@gmail.com>
2021-12-06 13:52:33 -0500

    day 6 part one done

---
+++ Day6/Day6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Advent
+{
+    internal static class Day6
+    {
+        private static void Main(string[] args)
+        {
+            List<int> fishies = new(); 
+            ReadUtil.ReadAndProcessInput(line =>
+            {
+                fishies.AddRange(line.Split(",").Select(fish => Convert.ToInt32(fish)));
+            });
+
+            Console.WriteLine($"Initial state: {string.Join(",", fishies)}");
+            for (var day = 1; day <= 256; day++)
+            {
+                for (var i = fishies.Count -1; i >= 0; i--)
+                {
+                    var cur = fishies[i];
+                    cur--;
+                    if (cur < 0)
+                    {
+                        cur = 6;
+                        fishies.Add(8);
+                    }
+
+                    fishies[i] = cur;
+                }
+                Console.WriteLine($"{day}");
+            }
+            Console.WriteLine($"Finished with {fishies.Count} fish.");
+        }
+    }
+}
+            
